9a5aa409f82a3fcd5757c914f284163ea1c2fe35
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-03-19 12:09:47 +0700

    pertemuan 5 - praktikum 2

---
+++ pertemuan 5/array_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device=width, initial-scale">
+    <title><title>
+</head>
+<body>
+    <?php
+       $Dosen = [
+          'nama' => 'Elok Nur Hamdana',
+          'domisili' => 'Malang',
+          'jenis_kelamin' => 'perempuan'];
+
+        echo "Nama : {$Dosen ['nama']} <br>";
+        echo "Domisili : {$Dosen ['domisili']} <br>";
+        echo "jenis kelamin : {$Dosen ['jenis_kelamin']} <br>";
+    ?>
+</body>
+</html>
