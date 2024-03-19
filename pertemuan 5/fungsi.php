33884b2e843fb61d98f03f112ef8dca2da83ad21
Andikaqodri <bagasmulyanuralif22@gmail.com>
2024-03-19 13:04:06 +0700

    pertemuan 5 - praktikum 3

---
+++ pertemuan 5/fungsi.php
@@ -0,0 +1,10 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Qodri<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+perkenalan();
+
+?>
